4bca4d9bc37021e927571c23a215f4485f91d6c1
Vinicius Gomes Beltrao <vinigbeltrao@gmail.com>
2023-10-01 18:35:21 -0300

    Mudança de nome na pasta views.py a fim de testar o repositorio

--- Budget/views.py
+++ Budget/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'global/home.html', context={
-        'name': 'Maria Augusta'
+        'name': 'Vinicius Beltrao'
     })
 
 
