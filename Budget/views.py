0f8fadfcb2a791feded00cb8f46bcb6781d8c80c
Maria Augusta <mabg@cesar.school>
2023-10-01 15:44:27 -0300

    Orçamento

--- Budget/views.py
+++ Budget/views.py
@@ -1,10 +1,12 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'global/home.html', context={
+        'name': 'Maria Augusta'
+    })
 
 
 def contact(request):
